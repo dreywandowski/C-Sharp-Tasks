8d34d512ab386827e9f31e6c60e0e96ddd64eaf7
dreysongz <aduramimo@gmail.com>
2020-04-22 16:31:05 +0100

    test

--- game.cs
+++ game.cs
@@ -11,74 +11,131 @@ namespace guessingGame
       
         public void Game()
         {
+            // Gets player name
             Console.WriteLine("Hello!! Welcome to the number guessing game. Please give us your name");
             string username = Console.ReadLine();
+
+            // Level type
             Console.WriteLine(username + " , welcome. Type E for Easy, M for Medium, and H for Hard");
             string level = Console.ReadLine();
+            
+            // random number generator
+              Random r = new Random();
 
+           
+        
           
-            switch (level)
-            {
+            switch (level){
+            
                 case "E":
-                    List<int> lives = new List<int>();
+                    Console.WriteLine("You have selected the easy level.");
+                     
+                     int guess = 1;
+                      // number of tries per level
+                int[] lives = new int[7];
+            int count = lives.Length;
 
-                    Console.WriteLine("You have selected the easy level. You get 6 lives left.  Now input a random number from 1 to 10");
-                    int randomNum = int.Parse(Console.ReadLine());
+              Console.WriteLine("You got 6 lives left.  Now input a random number from 1 to 10" + count);
 
-                    int[] livesE = new int[6];
-
-                    // generate a random number from 1 to 10
-                    Random r = new Random();
+                     do{
+           
+// generate a random number from 1 to 10
+                  
                    int newR = r.Next(1, 10);
 
-                    if(randomNum == newR)
-                    {
+                    int randomNum = int.Parse(Console.ReadLine());
+
+                
+                    if(randomNum == newR){
+                    
                         Console.WriteLine("You got it right!! You win :D");
-                    }
-                    else
-                    {
-                        lives.Add(randomNum);
-                        Console.WriteLine("That was wrong. :( . You chose: " + randomNum + " , "+ "Computer chose: "   + newR);
-                    }
+                        break;
+                     }
+
+                    else{
+                        Console.WriteLine("That was wrong. :( . You chose: " + randomNum  + " , "+ "Computer chose: "   + newR + "Try again");
+                       // guess++;
+                    } 
+         }
+                    
+                   while(guess < count);
+
+                        
+                    
                     break;
 
 
                 case "M":
-                    Console.WriteLine("You have selected the medium level. You get 5 lives in this level. Now input a random number from 1 to 20");
-                    int randNum = int.Parse(Console.ReadLine());
+                   Console.WriteLine("You have selected the Medium level.");
+                     
+                     int guessM = 1;
+                      // number of tries per level
+                int[] livesM = new int[5];
+            int countM = livesM.Length;
 
-                    // generate a random number from 1 to 20
-                    Random ra = new Random();
-                    int newRa = ra.Next(1, 20);
+              Console.WriteLine("You got 4 lives left.  Now input a random number from 1 to 20");
 
-                    if (randNum == newRa)
-                    {
-                        Console.WriteLine("You got it right!! :D");
-                    }
-                    else
-                    {
-                        Console.WriteLine("That was wrong. :( . You chose: " + randNum + " , " + "Computer chose: " + newRa);
-                    }
+                     do{
+           
+// generate a random number from 1 to 10
+                  
+                   int newR = r.Next(1, 20);
+
+                    int randomNum = int.Parse(Console.ReadLine());
+
+                
+                    if(randomNum == newR){
+                    
+                        Console.WriteLine("You got it right!! You win :D");
+                        break;
+                     }
+
+                    else{
+                        Console.WriteLine("That was wrong. :( . You chose: " + randomNum  + " , "+ "Computer chose: "   + newR + " "+ "Try again");
+                        //guess++;
+                    } 
+         }
+                    
+                   while(guessM < countM);
+
+
                     break;
               
 
                 case "H":
-                    Console.WriteLine("You have selected the hard level. Now input a random number from 1 to 50");
-                    int randNumb = int.Parse(Console.ReadLine());
+                    Console.WriteLine("You have selected the Hard level.");
+                     
+                     int guessH = 1;
+                      // number of tries per level
+                int[] livesH = new int[4];
+            int countH = livesH.Length;
 
-                    // generate a random number from 1 to 50
-                    Random ran = new Random();
-                    int newRan = ran.Next(1, 50);
+              Console.WriteLine("You got 3 lives left.  Now input a random number from 1 to 50");
 
-                    if (randNumb == newRan)
-                    {
-                        Console.WriteLine("You got it right!! :D");
-                    }
-                    else
-                    {
-                        Console.WriteLine("That was wrong. :( . You chose: " + randNumb + " , " + "Computer chose: " + newRan);
-                    }
-                    break;
+                     do{
+           
+// generate a random number from 1 to 10
+                  
+                   int newR = r.Next(1, 50);
+
+                    int randomNum = int.Parse(Console.ReadLine());
+
+                
+                    if(randomNum == newR){
+                    
+                        Console.WriteLine("You got it right!! You win :D");
+                        break;
+                     }
+
+                    else{
+                        Console.WriteLine("That was wrong. :( . You chose: " + randomNum  + " , "+ "Computer chose: "   + newR);
+                        //guess++;
+                    } 
+         }
+                    
+                   while(guessH < countH);
+                   break;
+
 
                 default:
                     Console.WriteLine("Please input a valid letter");
